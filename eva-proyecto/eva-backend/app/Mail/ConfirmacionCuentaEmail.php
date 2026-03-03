c978386c1d52977b48465bc27c3b1963f94fbfaf
= <=>
2026-03-03 15:13:56 -0500

    correo de confirmacion funcional

--- eva-proyecto/eva-backend/app/Mail/ConfirmacionCuentaEmail.php
+++ eva-proyecto/eva-backend/app/Mail/ConfirmacionCuentaEmail.php
@@ -22,13 +22,8 @@ class ConfirmacionCuentaEmail extends Mailable
     {
         $this->usuario = $usuario;
         $this->token = $token;
-        // Usar FRONTEND_URL del .env (debe estar configurado correctamente)
+        // Usar FRONTEND_URL del .env
         $frontendUrl = env('FRONTEND_URL');
-        
-        if (!$frontendUrl) {
-            \Log::error('FRONTEND_URL no está configurado en el archivo .env');
-            throw new \Exception('FRONTEND_URL no está configurado en el archivo .env');
-        }
         
         $this->urlConfirmacion = rtrim($frontendUrl, '/') . '/confirmar-cuenta/' . $token;
     }
